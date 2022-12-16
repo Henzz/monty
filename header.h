6af48307453b5ae25b52798b2566c874b42a98ca
Henzz <khenok53@gmail.com>
2022-12-16 05:26:30 +0300

    monty project on progress

---
+++ header.h
@@ -0,0 +1,71 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * struct var_s - to contain variables of the monty interpreter
+ * @queue: flag to determine if in stack or queue mode
+ * @stack_len: length of the stack
+ */
+typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+#define STACK 0
+#define QUEUE 1
+
+/* global struct to hold flag for queue and stack length */
+extern var_t var;
+
+/**
+ * struct stack_s - doubly linked list representation of a
+ *                  stack or queue
+ * @n: integer
+ * @prev: points to the previous element of the stack or queue
+ * @next: points to the next element of the stack or queue
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+stack_t *add_node(stack_t **stack, const int n);
+void get_op(char *op, stack_t **stack, unsigned int line_number);
+void m_push(stack_t **stack, unsigned int line_number);
+void m_pall(stack_t **stack, unsigned int line_number);
+void free_stack(int status, void *arg);
+void free_lineptr(int status, void *arg);
+void m_fs_close(int status, void *arg);
+
+#endif /* _HEADER_H_ */
