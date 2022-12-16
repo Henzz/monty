f4b33c658ad7f916e489c4d6876d24dbb4e84132
Henzz <khenok53@gmail.com>
2022-12-16 06:10:42 +0300

    moved monty files to file folder

--- header.h
+++ header.h
@@ -14,25 +14,7 @@
 #include <fcntl.h>
 
 /**
- * struct var_s - to contain variables of the monty interpreter
- * @queue: flag to determine if in stack or queue mode
- * @stack_len: length of the stack
- */
-typedef struct var_s
-{
-	int queue;
-	size_t stack_len;
-} var_t;
-
-#define STACK 0
-#define QUEUE 1
-
-/* global struct to hold flag for queue and stack length */
-extern var_t var;
-
-/**
- * struct stack_s - doubly linked list representation of a
- *                  stack or queue
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack or queue
  * @next: points to the next element of the stack or queue
@@ -60,6 +42,23 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct var_s - to contain variables of the monty interpreter
+ * @queue: flag to determine if in stack or queue mode
+ * @stack_len: length of the stack
+ */
+typedef struct var_s
+{
+	int queue;
+	size_t stack_len;
+} var_t;
+
+#define STACK 0
+#define QUEUE 1
+
+/* global struct to hold flag for queue and stack length */
+extern var_t var;
 
 stack_t *add_node(stack_t **stack, const int n);
 void get_op(char *op, stack_t **stack, unsigned int line_number);
